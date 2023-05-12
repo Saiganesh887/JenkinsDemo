10796efc2385b8c18c45165448d8eb10a736b6ec
Your Name <you@example.com>
2023-05-12 13:49:11 +0530

    first commit

--- src/main/java/com/example/jenkins/JenkinsApplication.java
+++ src/main/java/com/example/jenkins/JenkinsApplication.java
@@ -1,14 +1,13 @@
 package com.example.jenkins;
 
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.logging.Logger;
-
 @SpringBootApplication
 public class JenkinsApplication {
-	public static Logger logger = (Logger) LoggerFactory.getLogger(JenkinsApplication.class);
+	public static Logger logger = LoggerFactory.getLogger(JenkinsApplication.class);
 
 	public void init(){
 		logger.info("Application started");
--- src/test/java/com/example/jenkins/JenkinsApplicationTests.java
+++ src/test/java/com/example/jenkins/JenkinsApplicationTests.java
@@ -1,15 +1,15 @@
 package com.example.jenkins;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.logging.Logger;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class JenkinsApplicationTests {
-	public static Logger logger = (Logger) LoggerFactory.getLogger(JenkinsApplicationTests.class);
+	public static Logger logger = LoggerFactory.getLogger(JenkinsApplicationTests.class);
 
 	@Test
 	void contextLoads() {
